856a8063f58a9ed6264956738be06c6b9ee46f6b
Ziyao Du <791595877@qq.com>
2016-11-13 17:36:23 +0800

    2016/11/13

--- chaifen/ChaiFen.java
+++ chaifen/ChaiFen.java
@@ -5,11 +5,12 @@ public class ChaiFen
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
+		System.out.println("please input a string:");
 		String str = sc.next();
 		char[] ch = str.toCharArray();
-		char[] a = new char[10];
+		char[] a = new char[50];
 		int x = 0;
-		char[] s = new char[10];
+		char[] s = new char[50];
 		int y = 0;
 		for(int i = 0; i<ch.length; i++)
 		{
